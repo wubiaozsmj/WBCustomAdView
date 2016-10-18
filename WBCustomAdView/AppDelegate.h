e22f97d2d6b283e8c053dad04ea16170469c0207
wubiao <wubiao0313@163.com>
2016-10-18 09:52:36 +0800

    Initial Commit

---
+++ WBCustomAdView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WBCustomAdView
+//
+//  Created by 吴飚 on 2016/10/18.
+//  Copyright © 2016年 wubiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WBCustomAdView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WBCustomAdView
+//
+//  Created by 吴飚 on 2016/10/18.
+//  Copyright © 2016年 wubiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
